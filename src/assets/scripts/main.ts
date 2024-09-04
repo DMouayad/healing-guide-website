a45777984dc08f1105df462547bb0708181a6174
Mouayad <muayad.perun@outlook.com>
2024-09-04 18:06:15 +0300

    chore: extract global css & js to separate files

---
+++ src/assets/scripts/main.ts
@@ -0,0 +1,57 @@
+// navigate to section when a button with set attribute is clicked
+document.querySelectorAll("[section-link]").forEach((btn) => {
+    btn.addEventListener("click", function () {
+        document
+            .getElementById(btn.getAttribute("section-link") ?? "")
+            ?.scrollIntoView({ behavior: "smooth" });
+    });
+});
+// lazy loading background images
+const initialiseStyleBackgroundIntersectionObserver = () => {
+    const lazyBackgrounds = Array.from(
+        document.querySelectorAll("[data-background-image]"),
+    );
+
+    if (lazyBackgrounds.length === 0) {
+        return;
+    }
+
+    let lazyBackgroundObserver: IntersectionObserver;
+
+    const loadBackgroundIfElementOnScreen = (entry) => {
+        if (entry.isIntersecting) {
+            entry.target.style.backgroundImage = `url('${entry.target.dataset.backgroundImage}')`;
+            lazyBackgroundObserver.unobserve(entry.target);
+        }
+    };
+
+    const observeElementVisibility = (lazyBackground) => {
+        lazyBackgroundObserver.observe(lazyBackground);
+    };
+
+    const setBackground = (element) => {
+        element.style.backgroundImage = `url('${entry.target.dataset.backgroundImage}')`;
+    };
+
+    if (typeof window.IntersectionObserver === "function") {
+        lazyBackgroundObserver = new IntersectionObserver((entries) => {
+            entries.forEach(loadBackgroundIfElementOnScreen);
+        });
+        lazyBackgrounds.forEach(observeElementVisibility);
+    } else {
+        lazyBackgrounds.forEach(setBackground);
+    }
+};
+
+if (
+    typeof document.readyState === "string" &&
+    document.readyState === "complete"
+) {
+    initialiseStyleBackgroundIntersectionObserver();
+} else {
+    document.addEventListener(
+        "DOMContentLoaded",
+        initialiseStyleBackgroundIntersectionObserver,
+        true,
+    );
+}
